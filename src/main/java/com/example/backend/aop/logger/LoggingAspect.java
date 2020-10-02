eb4952ea6583aff930d062ea75917b28911413fb
minuk8932 <minuk8932@naver.com>
2020-10-02 14:19:11 +0900

    add AOP settings

---
+++ src/main/java/com/example/backend/aop/logger/LoggingAspect.java
@@ -0,0 +1,10 @@
+package com.example.backend.aop.logger;
+
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+
+@Component
+@Aspect
+public class LoggingAspect {
+
+}
