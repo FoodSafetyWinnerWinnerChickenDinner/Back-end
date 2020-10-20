a29289805d2cf35e534f8c3beddeb5c2739e7f83
minuk8932 <minuk8932@naver.com>
2020-10-20 15:38:36 +0900

    modify Repository Jpa -> Crud

--- src/main/java/com/example/backend/repositories/FoodRepository.java
+++ src/main/java/com/example/backend/repositories/FoodRepository.java
@@ -1,12 +1,12 @@
 package com.example.backend.repositories;
 
 import com.example.backend.models.Foods;
-import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface FoodRepository extends JpaRepository<Foods, Long> {
+public interface FoodRepository extends CrudRepository<Foods, Long> {
     @Query("SELECT f from Foods as f WHERE f.foodName = ?1 AND f.category = ?2 AND f.total = ?3")
     Foods findByNameAndCategory(String name, String category, double total);
 
