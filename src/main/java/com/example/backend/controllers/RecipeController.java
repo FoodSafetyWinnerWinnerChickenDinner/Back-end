2121e64bb8793af97a116c48ca96f9fcfbd9f572
minuk8932 <minuk8932@naver.com>
2020-11-12 16:19:10 +0900

    add RecipeController

---
+++ src/main/java/com/example/backend/controllers/RecipeController.java
@@ -0,0 +1,12 @@
+package com.example.backend.controllers;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin
+@RequestMapping("recipes")
+public class RecipeController {
+    
+}
