cea94d3a2fe662458a55a92ef5394c2f2ec4fd7f
minuk8932 <minuk8932@naver.com>
2021-07-17 16:43:22 +0900

    Refactor: package configurations

--- src/main/java/com/example/backend/configurations/OpenApiConfig.java
+++ src/main/java/com/example/backend/configurations/OpenApiConfig.java
@@ -1,13 +1,12 @@
 package com.example.backend.configurations;
 
 import lombok.Getter;
-import lombok.Setter;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Component;
 
 @Component
-@Getter @Setter
+@Getter
 @PropertySource("classpath:openapi.properties")
 public class OpenApiConfig {
     @Value("${apiAuthKey}")
